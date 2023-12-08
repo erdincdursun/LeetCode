829aa306cba460ff2a33ce6d34588156f714107e
Erdinç Dursun <erdincdursun20@gmail.com>
2023-12-08 06:48:01 +0300

    Initial commit

---
+++ MergeStringsAlternately/Program.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualBasic;
+using System.Text;
+
+namespace MergeStringsAlternately
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region QuestionDescription
+            //You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+            //Return the merged string.
+            //Example 1:
+
+            //Input: word1 = "abc", word2 = "pqr"
+            //Output: "apbqcr"
+            //Explanation: The merged string will be merged as so:
+            //word1: a b   c
+            //word2:    p q   r
+            //merged: a p b q c r
+            //Example 2:
+
+            //Input: word1 = "ab", word2 = "pqrs"
+            //Output: "apbqrs"
+            //Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+            //word1: a b
+            //word2: p q   r s
+            //merged: a p b q   r s
+            #endregion
+
+            string word1 = "ab c";
+            string word2 = " pq r";
+            //result = apbqcr 
+            Console.Write(MergeStrings(word1, word2)); 
+
+        }
+
+        public static string MergeStrings(string word1, string word2)
+        {
+            int i = 0, j = 0;
+            int len1 = word1.Length, len2 = word2.Length;
+            StringBuilder merged = new StringBuilder();
+
+            while (i < len1 && j < len2)
+            {
+                merged.Append(word1[i]);
+                merged.Append(word2[j]);
+                i++;
+                j++;
+            }
+
+            // Append remaining characters from word1, if any
+            while (i < len1)
+            {
+                merged.Append(word1[i]);
+                i++;
+            }
+
+            // Append remaining characters from word2, if any
+            while (j < len2)
+            {
+                merged.Append(word2[j]);
+                j++;
+            }
+
+            return merged.ToString();
+        }
+    }
+
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,61 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics;
+using System.Runtime.Intrinsics.X86;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region QuestionDescription
+            //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.You may assume that each input would have exactly one solution, and you may not use the same element twice.You can return the answer in any order.Example 1:
+
+            //Input: nums = [2, 7, 11, 15], target = 9
+            //Output: [0,1]
+            //Explanation: Because nums[0] +nums[1] == 9, we return [0, 1].
+            //Example 2:
+
+            //Input: nums = [3, 2, 4], target = 6
+            //Output: [1,2]
+            //Example 3:
+
+            //Input: nums = [3, 3], target = 6
+            //Output: [0,1]
+            #endregion
+
+            int[] nums = { 5, 1, 4, 15 };
+            int target = 9;
+
+            int[] array = TwoSum(nums, target);
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> hashTable = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+
+                if (hashTable.ContainsKey(complement))
+                {
+                    return new int[] { hashTable[complement], i };
+                }
+
+                if (!hashTable.ContainsKey(nums[i]))
+                {
+                    hashTable[nums[i]] = i;
+                }
+            }
+
+            return new int[] { };
+        }
+    }
+}
